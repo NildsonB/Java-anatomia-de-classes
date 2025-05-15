e3fd7da917e188d56c38df4d5c7449ded149fe79
Nildson <nildsonb15@gmail.com>
2025-05-14 21:37:57 -0300

    Teste com tipos numericos

--- src/dio/java/sintaxe_basica/sintaxe_java/BoletinEstudantil.java
+++ src/dio/java/sintaxe_basica/sintaxe_java/BoletinEstudantil.java
@@ -1,17 +1,22 @@
 package dio.java.sintaxe_basica.sintaxe_java;
-public class BoletinEstudantil {
-    public static void main(String[] args) {
-        
-        int mediaFinal = 9;
+    public class BoletinEstudantil {
+        public static void main(String[] args) {
+        /**
+            int mediaFinal = 9;
 
-        if(mediaFinal<6)
-            System.out.println("REPROVADO");
-        else if(mediaFinal==6)
-            System.out.println("PROVA MINERVA");
-        else
-            System.out.println("Aprovado");
+            if(mediaFinal<6)
+                System.out.println("REPROVADO");
+            else if(mediaFinal==6)
+                System.out.println("PROVA MINERVA");
+            else
+                System.out.println("Aprovado");*/
 
-}
+
+            short nomeroCurto = 1;
+            int nomeroNormal = nomeroCurto;
+            short numeroCurto2 = (short) nomeroNormal;
+
+        }
 
 
 }
