5fa9a3500bdb5fdbbe9e8419033c8864eff28452
Nildson <nildsonb15@gmail.com>
2025-05-22 21:47:36 -0300

    Script teste de condicional composta com scanner

---
+++ src/dio/java/sintaxe_basica/estruturas_condicionais_introducao/ResultadoEscolar.java
@@ -0,0 +1,20 @@
+package dio.java.sintaxe_basica.estruturas_condicionais_introducao;
+
+import java.util.Scanner;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Insira a nota do(a) aluno(a): ");
+        double nota = scanner.nextDouble();
+
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
