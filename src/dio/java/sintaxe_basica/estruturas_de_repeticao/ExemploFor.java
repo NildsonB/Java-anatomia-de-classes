e32bc8e9333da2b22e47b2d7d90b36625b082cad
Nildson <nildsonb15@gmail.com>
2025-05-23 22:17:14 -0300

    Exercício para entendimento de For

---
+++ src/dio/java/sintaxe_basica/estruturas_de_repeticao/ExemploFor.java
@@ -0,0 +1,9 @@
+package dio.java.sintaxe_basica.estruturas_de_repeticao;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+    }
+}
