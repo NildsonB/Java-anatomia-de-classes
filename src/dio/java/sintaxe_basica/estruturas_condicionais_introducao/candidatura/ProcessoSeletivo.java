7d8235181e9dcdbe8306ecd22ea9f74093ee4b83
Nildson <nildsonb15@gmail.com>
2025-05-29 23:08:53 -0300

    Inserção de método para contato com candidatos

--- src/dio/java/sintaxe_basica/estruturas_condicionais_introducao/candidatura/ProcessoSeletivo.java
+++ src/dio/java/sintaxe_basica/estruturas_condicionais_introducao/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,27 @@
 package dio.java.sintaxe_basica.estruturas_condicionais_introducao.candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        //selecaoCandidatos();
+        //imprimirSelecionados();
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        System.out.println("Imperimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de Nº " + (indice+1) + " é o " + candidatos[indice]);
+        }
+    }
+
     static void selecaoCandidatos() {
         String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Danilea", "Joana"};
 
@@ -24,6 +40,28 @@ public class ProcessoSeletivo {
             candidatosAtual++;
         }
     }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+        }while(continuarTentando && tentativasRealizadas <= 3);
+        if(atendeu)
+            System.out.println("Conseguimos contato com o candidato " + candidato + " na tentativa " + tentativasRealizadas);
+        else
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentaticas atingido");
+    }
+
+    // Método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
