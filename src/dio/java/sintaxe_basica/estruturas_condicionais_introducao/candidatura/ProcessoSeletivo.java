bb0f21b0c2f9cb914389e875daff2c7f9cffd1ff
Nildson <nildsonb15@gmail.com>
2025-05-29 22:12:03 -0300

    Seleção dos candidatos a partir de uma lista

--- src/dio/java/sintaxe_basica/estruturas_condicionais_introducao/candidatura/ProcessoSeletivo.java
+++ src/dio/java/sintaxe_basica/estruturas_condicionais_introducao/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,31 @@
 package dio.java.sintaxe_basica.estruturas_condicionais_introducao.candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Danilea", "Joana"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato (double salarioPretendido) {
